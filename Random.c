884c47c0f96114e29fd15257a36f9c066c68c1ee
root <root@DESKTOP-KD4NRAS.localdomain>
2023-09-15 19:43:29 -0600

    Detalles

--- Random.c
+++ Random.c
@@ -5,8 +5,8 @@
 
 int main(){
 
-    if (sodium_init() == -1) {
+    if (sodium_init() == -1)
         return 1;
-    }
+        
     printf("%u <---- Número obtenido\n\n",randombytes_random());          // Retorna un valor aleatorio entre 0x0 and 0xffffffff
 }
